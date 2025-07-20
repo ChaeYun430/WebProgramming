73791faad76b8db5a484d67f337dbf564aad1798
marchlcy@gmail.com <marchlcy@gmail.com>
2025-07-21 01:06:01 +0900

    2. simple CRUD

--- BootBoard/src/main/java/com/lcy/bootboard/domain/BaseEntity.java
+++ BootBoard/src/main/java/com/lcy/bootboard/domain/BaseEntity.java
@@ -1,14 +1,29 @@
 package com.lcy.bootboard.domain;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.EntityListeners;
 import jakarta.persistence.MappedSuperclass;
 import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
 
 @MappedSuperclass
 @EntityListeners(value = {AuditingEntityListener.class})//엔티티가 데이터베이스에 추가되거나 변경될 때 자동으로 시간 값 지정 //프로젝트 설정에 @EnableJpaAuditing 추가
 @Getter
 public class BaseEntity {
+
+    @CreatedDate    //Entity가 생성되어 저장될 때 시간이 자동으로 저장
+    @Column(name = "regiDate", updatable = false)
+    private LocalDateTime regiDate;
+
+    @LastModifiedDate    //조회한 Entity의 값을 변경할 때 시간이 자동으로 저장
+    @Column(name = "modiDate")
+    private LocalDateTime modiDate;
+
+
 
 
 
--- BootBoard/src/test/java/com/lcy/bootboard/repository/BoardRepositoryTests.java
+++ BootBoard/src/test/java/com/lcy/bootboard/repository/BoardRepositoryTests.java
@@ -16,6 +16,7 @@ public class BoardRepositoryTests {
     @Autowired
     private BoardRepository boardRepository;
     //boardRepository 메서드 실행 결과 리턴 타입에 집중
+    //JPA를 이용한다 == 영속 콘텍스트와 데이터베이스를 동기화해서 관리한다
 
     //Builder : 배열이나 컬렉션을 통한 생성이 아닌 직접 원하는 값을 넣어 생성 가능, build 메서드를 통해 스트림을 리턴
     @Test
@@ -36,7 +37,26 @@ public class BoardRepositoryTests {
     public void testSelect(){
         Long bno = 100L;
         Optional<Board> result = boardRepository.findById(bno);
-        Board board = result.orElseThrow();
+        Board board = result.orElseThrow(); //Optional의 인자가 null일 경우 예외처리를 시킨다.
         log.info(board);
     }
+
+    @Test
+    public void testUpdate(){
+        Long bno = 100L;
+        Optional<Board> result = boardRepository.findById(bno);
+        Board board = result.orElseThrow();
+        board.change("update 100 title", "update 100 content");
+        boardRepository.save(board);
+        //조회와 입력 활용
+    }
+
+    @Test
+    public void testDelete(){
+        Long bno = 100L;
+        boardRepository.deleteById(bno);
+    }
+    //https://ittrue.tistory.com/254  읽어보기... 지금은 힘드러어어어
+
+    
 }
