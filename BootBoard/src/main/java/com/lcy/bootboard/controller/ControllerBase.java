d2502b9d9742eb19d2a801abbc96170569925398
220-18\LeeChaeYun <marchlcy@gmail.com>
2025-07-20 17:53:53 +0900

    1. JPA, dataSource, log, (test)lombok configuration & Controller, html(Thymeleaf)

---
+++ BootBoard/src/main/java/com/lcy/bootboard/controller/ControllerBase.java
@@ -0,0 +1,30 @@
+package com.lcy.bootboard.controller;
+
+import lombok.extern.log4j.Log4j2;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@Log4j2
+public class ControllerBase {
+
+    //Thymeleaf 이용
+    @GetMapping("/base")
+    public void base(Model model){
+        log.info("base");
+        model.addAttribute("msg", "Wellcome Controller");
+    }
+
+    //API서버(순수한 데이터만 전송) 제작
+    //JSON (JavaScript Object Notation) : 구조를 가진 데이터(객체)를 자바스크립트의 "객체표기법"으로 표현한 순수한 문자열
+    //스프링에서는 jackson-databind라는 별도의 라이브러리 추가 후 개발
+    //부트는 web항목을 추가할 때 자동으로 포함됨
+    @GetMapping("/baseArr")
+    public String[] baseArr(){
+        log.info("baseArr..............");
+        return new String[]{"AAA", "BBB", "CCC"};
+    }
+    //서버에서 해당 데이터는 "application/json"으로 전송
+
+}
---
+++ BootBoard/src/test/java/com/lcy/bootboard/repository/BoardRepositoryTests.java
@@ -0,0 +1,42 @@
+package com.lcy.bootboard.repository;
+
+import com.lcy.bootboard.domain.Board;
+import lombok.extern.log4j.Log4j2;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.Optional;
+import java.util.stream.IntStream;
+
+@SpringBootTest
+@Log4j2
+public class BoardRepositoryTests {
+
+    @Autowired
+    private BoardRepository boardRepository;
+    //boardRepository 메서드 실행 결과 리턴 타입에 집중
+
+    //Builder : 배열이나 컬렉션을 통한 생성이 아닌 직접 원하는 값을 넣어 생성 가능, build 메서드를 통해 스트림을 리턴
+    @Test
+    public void testInsert(){
+        IntStream.rangeClosed(1, 100).forEach(
+                i->{
+                    Board board = Board.builder()
+                            .title("제목 : " + i)
+                            .content("내용 : " + i)
+                            .writer("작성자 : " + (i % 10)).build();
+                    Board result = boardRepository.save(board);
+                    log.info("bno" + result.getBno());
+                }
+        );
+    }
+
+    @Test
+    public void testSelect(){
+        Long bno = 100L;
+        Optional<Board> result = boardRepository.findById(bno);
+
+
+    }
+}
